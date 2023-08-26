f6a4c6bafe19f44802c0a08434329faaf8e5b2a4
ergegananputra <ergegananputra@gmail.com>
2023-08-26 23:54:12 +0700

    Tugas Pertemuan 2 - Login Page

--- app/src/main/java/com/example/praktikumpemrogramanbergerakandroid/MainActivity.kt
+++ app/src/main/java/com/example/praktikumpemrogramanbergerakandroid/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.praktikumpemrogramanbergerakandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.praktikumpemrogramanbergerakandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+
+            buttonLogin.setOnClickListener {
+                val uname : String = usernameInput?.text.toString()
+                val pwd : String = userPassword.text.toString()
+
+                if (uname.isNotEmpty() && pwd.isNotEmpty()) {
+                    Toast.makeText(this@MainActivity, "Login Sukses", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
     }
 }
